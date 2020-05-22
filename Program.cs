9345f1903ded0bcb1e9190a47f73464336c5fdbf
João Pedro <jpedrodss@gmail.com>
2020-05-22 10:47:36 -0300

    repositorio iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
